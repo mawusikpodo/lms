cea98f36b1d526955867726123886ff633a31217
mawusikpodo <mawusikpodo@yahoo.com>
2023-12-21 14:10:24 +0000

    added folder structure

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import Sidebar from "./_components/sidebar";
+
+const AuthLayout = ({children}: {children: ReactNode}) => {
+    return ( 
+        <div className="h-full">
+            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+                <Sidebar />
+            </div>
+            <main className="md:pl-56 h-full">
+                {children}
+            </main>
+        </div>
+     );
+}
+ 
+export default AuthLayout;
